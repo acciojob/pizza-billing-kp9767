b96078f5fab0cbcff03410ab7172c625cee173fb
unknown <kalpeshpatil19999@gmail.com>
2023-08-25 16:18:06 +0530

    pizza-billing

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,62 @@
 package com.driver;
 
+import java.net.StandardSocketOptions;
+
 public class Pizza {
 
     private int price;
     private Boolean isVeg;
     private String bill;
 
+
     public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
+        this.isVeg=isVeg;
+        if(isVeg==true) this.price=300;
+        else this.price=400;
+
     }
 
     public int getPrice(){
         return this.price;
     }
-
+    boolean cheese=true;
     public void addExtraCheese(){
         // your code goes here
+        if(cheese==true) {
+            price = price + 80;
+            System.out.println("Extra Cheese Added: 80");
+        cheese=false;
+        }
     }
-
+    boolean topping=true;
     public void addExtraToppings(){
         // your code goes here
+        if(cheese==false) {
+            if (topping == true) {
+                if (isVeg == true) {
+                    System.out.println("Extra Toppings Added: 70");
+                    price += 70;
+                } else {
+                    System.out.println("Extra Toppings Added: 120");
+                    price += 120;
+                }
+                topping = false;
+            }
+        }
     }
-
-    public void addTakeaway(){
+    boolean take=true;
+   public void addTakeaway(){
         // your code goes here
+       if(take==true){
+        price+=20;
+        System.out.println("Paperbag Added: 20");
+        take=false;
+       }
+
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+         return ("Total price: "+price);
     }
 }
